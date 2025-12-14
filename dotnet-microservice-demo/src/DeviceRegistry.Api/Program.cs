59ab1ba4677fcbe9552abeac7a4603d87a812389
Nazgul Engvall <192051880+nazgulengvall@users.noreply.github.com>
2025-12-14 19:29:01 +0100

    Register IDeviceRepository in DI (AddScoped)

--- dotnet-microservice-demo/src/DeviceRegistry.Api/Program.cs
+++ dotnet-microservice-demo/src/DeviceRegistry.Api/Program.cs
@@ -1,10 +1,13 @@
 using DeviceRegistry.Api.Data;
+using DeviceRegistry.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IDeviceRepository, DeviceRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
